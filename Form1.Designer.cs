75751df18dc1e69b8eefbcb48dfb814873b361bc
sungman ko <33561581+sungmanko@users.noreply.github.com>
2020-01-06 09:23:21 +0900

    初期版コミット

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DragAndDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.label1 = new System.Windows.Forms.Label();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewControl1 = new System.Windows.Forms.PrintPreviewControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Font = new System.Drawing.Font("メイリオ", 12F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(12, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(262, 604);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.ListBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.ListBox1_DragEnter);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(215, 662);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(59, 56);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(292, 12);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(704, 706);
+            this.axAcroPDF1.TabIndex = 3;
+            this.axAcroPDF1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 12F);
+            this.label1.Location = new System.Drawing.Point(11, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "対象ファイル一覧";
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewControl1
+            // 
+            this.printPreviewControl1.Location = new System.Drawing.Point(292, 12);
+            this.printPreviewControl1.Name = "printPreviewControl1";
+            this.printPreviewControl1.Size = new System.Drawing.Size(704, 706);
+            this.printPreviewControl1.TabIndex = 5;
+            this.printPreviewControl1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.printPreviewControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "　会員サポートシステム開発";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.Label label1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewControl printPreviewControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DragAndDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //ListBox1のDragEnterイベントハンドラ
+        private void ListBox1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            //コントロール内にドラッグされたとき実行される
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                //ドラッグされたデータ形式を調べ、ファイルのときはコピーとする
+                e.Effect = DragDropEffects.Copy;
+            else
+                //ファイル以外は受け付けない
+                e.Effect = DragDropEffects.None;
+        }
+
+        //ListBox1のDragDropイベントハンドラ
+        private void ListBox1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            //コントロール内にドロップされたとき実行される
+            //ドロップされたすべてのファイル名を取得する
+            string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //ListBoxに追加する
+            listBox1.Items.AddRange(fileName);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                DialogResult dr = MessageBox.Show("指定したファイルを削除しますか？", "【ファイル削除】", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+                if (dr == DialogResult.OK)
+                {
+                    System.IO.File.Delete(listBox1.SelectedItem.ToString());
+                    listBox1.Items.Remove(listBox1.SelectedItem);
+                }
+            }
+            else
+            {
+                MessageBox.Show("選択されたアイテムがありません。", "【ファイル削除】", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            // 選択したファイルが存在する場合
+            if (listBox1.SelectedItem != null)
+            {
+                // 拡張子を取得
+                string extensionKey = System.IO.Path.GetExtension(listBox1.SelectedItem.ToString()).ToLower();
+
+                switch (extensionKey)
+                {
+                    case ".pdf":     // PDFファイル
+                        // ブラウザコントロールの作成
+                        axAcroPDF1.Visible = true;
+                        printPreviewControl1.Visible = false;
+
+                        axAcroPDF1.LoadFile(listBox1.SelectedItem.ToString());
+                        break;
+
+                    case ".csv":     // CSVファイル
+                        axAcroPDF1.Visible = false;
+                        printPreviewControl1.Visible = true;
+
+                        printPreviewControl1.Document = printDocument1;
+
+                        break;
+
+                    case ".xlsx":    // EXCELファイル
+                        axAcroPDF1.Visible = false;
+                        printPreviewControl1.Visible = true;
+
+                        break;
+
+                    default:
+                        axAcroPDF1.Visible = false;
+                        printPreviewControl1.Visible = false;
+
+                        /* 特定できてない拡張子が来た場合を想定 */
+                        break;
+                }
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            //Graphics g = e.Graphics;
+            //PointF drawPoint = new PointF(150.0F, 150.0F);
+            //using (Font font = new Font("Lucida Console", 30))
+            //using (SolidBrush drawBrush = new SolidBrush(Color.Black))
+            //{
+            //    g.DrawString("printPreviewControl test.....\n printer", font, drawBrush, drawPoint);
+            //}
+
+            //画像を読み込む
+            Image img = Image.FromFile(listBox1.SelectedItem.ToString());
+            //画像を描画する
+            e.Graphics.DrawImage(img, 0, 0, img.Width, img.Height);
+            //次のページがないことを通知する
+            e.HasMorePages = false;
+            //後始末をする
+            img.Dispose();
+        }
+    }
+}
